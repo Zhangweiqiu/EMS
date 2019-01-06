3c202b6320d9b6e11f2f671814d445ae244fddd3
Yve <ppsxxn@qq.cocm>
2019-01-06 14:50:56 +0800

    第五次提交

--- src/nit/User.java
+++ src/nit/User.java
@@ -77,19 +77,21 @@ public class User {
 			System.out.println("*\t\t\t\t*");
 			System.out.println("*\t1.进行课程管理\t\t*");
 			System.out.println("*\t2.查看学生成绩\t\t*");
+			System.out.println("*\t3.修改用户密码\t\t*");
 			System.out.println("*\t\t\t\t*");
 			System.out.println("*********************************");	
 			int x = sc.nextInt();
+			Connection con = null;
+			String driver = "com.mysql.jdbc.Driver";
+			String url = "jdbc:mysql://localhost:3306/mytest";
+			String user = "root";
+			String password1 = "a1s2d3f4";
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password1);
 			if (x == 1) {
 				System.out.println("您要进行的是：1.增加课程 2.删除课程");
 				int y = sc.nextInt();
-				Connection con = null;
-				String driver = "com.mysql.jdbc.Driver";
-				String url = "jdbc:mysql://localhost:3306/mytest";
-				String user = "root";
-				String password1 = "a1s2d3f4";
-				Class.forName(driver);
-				con = DriverManager.getConnection(url, user, password1);
+				//
 				PreparedStatement psql;
 				String another = sc.nextLine();
 				if (y == 1) {
@@ -110,7 +112,12 @@ public class User {
 				}
 				if (y == 2) {
 					Statement statement = con.createStatement();
-					ResultSet rs = statement.executeQuery("select * from course");
+					//方法1
+					String str = "select * from course where Teacher = '%s'";
+					String msg = String.format(str, userName);
+					//方法2
+					ResultSet rs = statement.executeQuery("select * from course where Teacher =  '"+userName+"'");
+					//"select * from course where Teacher =  '"+userName+"'";
 				
 						
 					System.out.println("-----------------");
@@ -135,6 +142,63 @@ public class User {
 					psql.close();
 				}
 			}
+			else if (x == 2) {
+				System.out.println("*********************************");
+				System.out.println("*\t\t\t\t*");
+				System.out.println("*\t1.按课程查询\t*");
+				System.out.println("*\t2.按学生查询\t*");
+				System.out.println("*\t\t\t\t*");
+				System.out.println("*********************************");
+				int y = sc.nextInt();
+				String ano = sc.nextLine();
+				if (y == 1) {
+					Statement statement = con.createStatement();
+					System.out.println("请输入要查询的课程的ID：");
+					int courseID = sc.nextInt();
+					ResultSet rs = statement.executeQuery("select * from score where CourseID =  '"+courseID+"'");
+					System.out.println("-----------------");
+					System.out.println("该课程的成绩为：");
+					System.out.println("-----------------");
+					int studentNo = 0;
+					float score = 0.0f;
+					while (rs.next()) {
+						studentNo = rs.getInt("StudentNo");
+						score = rs.getFloat("Score");
+						System.out.println("课程ID：" + courseID + "\t学生学号：" + studentNo + "\t成绩：" + score);
+					}
+					rs.close();
+				}
+				else if (y == 2) {
+					Statement statement = con.createStatement();
+					System.out.println("请输出要查询的学生的学号：");
+					String studentno = sc.nextLine();
+					ResultSet rs = statement.executeQuery("select * from score where StudentNo =  '"+studentno+"'");
+					System.out.println("-----------------");
+					System.out.println("该学生的成绩为：");
+					System.out.println("-----------------");
+					int courseID = 0;
+					float score = 0.0f;
+					while (rs.next()) {
+						courseID = rs.getInt("CourseID");
+						score = rs.getFloat("Score");
+						System.out.println("课程ID：" + courseID + "\t成绩：" + score);
+					}
+					rs.close();
+				}
+				else System.out.println("请进行正确的操作！");
+			}
+			else if (x == 3) {
+				String another = sc.nextLine();
+				System.out.println("请输入新的密码：");
+				String newPassword = sc.nextLine();
+				PreparedStatement sql;
+				sql = con.prepareStatement("update user set Password = ? where UserName = ? ");
+				sql.setString(1, newPassword);
+				sql.setString(2, userName);
+				sql.executeUpdate();
+				System.out.println("修改密码成功！");
+			}
+			else System.out.println("请进行正确的操作！");
 			}
 		}
 	}
